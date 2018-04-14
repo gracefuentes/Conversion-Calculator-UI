48ce6f5843782fc86a0d78637d228503529b67b5
sheyla fuentes <sgf763@mail.missouri.edu>
2018-04-13 20:17:06 -0500

    leyoutready

---
+++ Conversion Calculator UI/Conversion Calculator UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Conversion Calculator UI
+//
+//  Created by Sheyla Astete on 4/12/18.
+//  Copyright © 2018 tech innovator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
